d3713ab23ba797d2abd2f67b226c97cb56abb9af
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-03-06 01:17:51 +0300

    finished lab4

---
+++ src/Lab4/Main.java
@@ -0,0 +1,32 @@
+package Lab4;
+
+public class Main {
+    public static void main(String[] args) {
+        SortedIntegerList listWithoutDuplicates = new SortedIntegerList(false);
+        SortedIntegerList listWithDuplicates = new SortedIntegerList(true);
+
+        listWithoutDuplicates.add(1);
+        listWithoutDuplicates.add(1);
+        listWithoutDuplicates.add(2);
+        listWithoutDuplicates.add(3);
+
+        listWithDuplicates.add(1);
+        listWithDuplicates.add(1);
+        listWithDuplicates.add(2);
+        listWithDuplicates.add(3);
+
+        System.out.println("List1 (without duplicates) :" + listWithoutDuplicates.toString());
+        System.out.println("List2 (with duplicates) :" + listWithDuplicates.toString());
+
+        System.out.println("List1 equals List2: " + listWithDuplicates.equals(listWithoutDuplicates));
+
+        listWithoutDuplicates.remove(1);
+        System.out.println("List1 after remove 1: " + listWithoutDuplicates.toString());
+
+        listWithDuplicates.remove(1);
+        listWithDuplicates.remove(1);
+        System.out.println("List2 after double remove 1: " + listWithDuplicates.toString());
+
+        System.out.println("List1 equals List2: " + listWithDuplicates.equals(listWithoutDuplicates));
+    }
+}
---
+++ src/Lab4/SortedIntegerList.java
@@ -0,0 +1,70 @@
+package Lab4;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class SortedIntegerList {
+    private final boolean possibilityDuplicates;
+    private final LinkedList<Integer> data;
+
+    public SortedIntegerList(boolean possibilityDuplicates){
+        this.possibilityDuplicates = possibilityDuplicates;
+        this.data = new LinkedList<Integer>();
+    }
+
+    public void add(int number){
+        ListIterator<Integer> it = data.listIterator();
+        while (it.hasNext()) {
+            Integer cur = it.next();
+            if (cur > number) {
+                it.previous();
+                it.add(number);
+                return;
+            }
+            if (cur == number && !possibilityDuplicates) return;
+        }
+        it.add(number);
+    }
+
+    public void remove(int number){
+        this.data.removeFirstOccurrence(number);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+
+        if (obj == this) return true;
+
+        if (!(obj instanceof SortedIntegerList)) return false;
+
+
+        SortedIntegerList tmp = (SortedIntegerList) obj;
+
+        if (this.data.size() != tmp.data.size()) return false;
+
+        ListIterator<Integer> iterator1 = this.data.listIterator();
+        ListIterator<Integer> iterator2 = tmp.data.listIterator();
+
+        while (iterator1.hasNext()) {
+            if (!(iterator1.next().equals(iterator2.next()))) return false;
+        }
+        return true;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (Integer i : data) {
+            sb.append(i);
+            sb.append(", ");
+        }
+
+        if (sb.length() > 1) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
+}
