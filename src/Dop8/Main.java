709e6c924c6726513fb201223d7dbe77da10d6f6
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-04-10 00:34:51 +0300

    minor changes in Main

--- src/Dop8/Main.java
+++ src/Dop8/Main.java
@@ -5,16 +5,18 @@ public class Main {
         long start1 = System.currentTimeMillis();
         Backpack backpack = new Backpack(100);
         backpack.solve(0, 100, 0);
+        System.out.print("Max price:");
         backpack.printPriceBestBackpack();
         long end1 = System.currentTimeMillis();
         long time = (end1 - start1);
-        System.out.println("time: " + time + " ms");
+        System.out.println("1 Thread: time: " + time + " ms");
 
         long start2 = System.currentTimeMillis();
+        System.out.print("Max price:");
         System.out.println(backpack.solveWithThreads(7));
         long end2 = System.currentTimeMillis();
         time = (end2 - start2);
-        System.out.println("time: " + time + " ms");
+        System.out.println("7 Thread: time: " + time + " ms");
     }
 
 }
