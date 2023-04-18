51b4077452ff619dfd927a65510ae5a7ab484e7f
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-04-18 20:02:14 +0300

    minor changes in Main

--- src/Dop8/Main.java
+++ src/Dop8/Main.java
@@ -3,8 +3,9 @@ package Dop8;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         long start1 = System.currentTimeMillis();
-        Backpack backpack = new Backpack(100);
-        backpack.solve(0, 100, 0);
+        int maxWeight = 70;
+        Backpack backpack = new Backpack(maxWeight);
+        backpack.solve(0, maxWeight, 0);
         System.out.print("Max price:");
         backpack.printPriceBestBackpack();
         long end1 = System.currentTimeMillis();
@@ -12,11 +13,12 @@ public class Main {
         System.out.println("1 Thread: time: " + time + " ms");
 
         long start2 = System.currentTimeMillis();
+        int countOfThreads = 7;
         System.out.print("Max price:");
-        System.out.println(backpack.solveWithThreads(7));
+        System.out.println(backpack.solveWithThreads(countOfThreads));
         long end2 = System.currentTimeMillis();
         time = (end2 - start2);
-        System.out.println("7 Thread: time: " + time + " ms");
+        System.out.println(countOfThreads + " Thread: time: " + time + " ms");
     }
 
 }
