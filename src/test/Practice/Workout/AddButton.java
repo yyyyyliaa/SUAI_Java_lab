54ae521dc13b81c68c826fb8a12f2cb0094c1976
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-07-07 18:59:27 +0300

    fixed add day bug

--- src/test/Practice/Workout/AddButton.java
+++ src/test/Practice/Workout/AddButton.java
@@ -6,26 +6,37 @@ import java.util.HashSet;
 
 public class AddButton extends JButton {
 
-    public AddButton(String text){
+    public AddButton(String text){ //TODO: Сделать при добавлении дня всплывающее окно с выбором вида упражнений
         super(text);
     }
 
-    public void addActionListener(HashSet<String> days, HashMap<String, TrainingDay> trDays, Workout w){
+    private boolean checkDataFormat(int d, int m){
+        return (m != 4 && m != 6 && m != 9 && m != 11 && m != 2 && d>0 && d<31) ||
+                (m == 2 && d < 29 && d>0) ||
+                ((m == 4 || m == 6 || m == 9 || m == 11) && d < 32 && d > 0);
+    }
+
+    public void addActionListener(HashSet<String> days, HashMap<String, TrainingDay> trDays, JFrame w){
         super.addActionListener(e -> {
             String data;
             data = JOptionPane.showInputDialog ("Enter the date in the format: dd/mm");
+            int d, m;
 
-            if(data.charAt(2) != '/'){
-                JOptionPane.showMessageDialog(w,"Wrong data");
-            } else {
+            if(data.charAt(2) == '/'){
                 String[] tokens = data.split("/");
-                int d = Integer.parseInt(tokens[0]);
-                int m = Integer.parseInt(tokens[1]);
-                if(d>0 && d<32 && m>0 && m<13){
-                    days.add(data);
-                    trDays.put(data, new TrainingDay(d, m));
-                } else JOptionPane.showMessageDialog(w,"Wrong data");
+                d = Integer.parseInt(tokens[0]);
+                m = Integer.parseInt(tokens[1]);
+            } else {
+                JOptionPane.showMessageDialog(w,"Wrong data");
+                return;
+            }
+
+            if(checkDataFormat(d, m)){
+                days.add(data);
+                trDays.put(data, new TrainingDay(d, m));
             }
         });
+
+
     }
 }
